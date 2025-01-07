d9e098e22470b3ae1e0a4eb5c5206581dce2ca19
Ogaga <iyaraogaga69@gmail.com>
2025-01-07 12:34:14 +0100

    footer

--- src/types/works.ts
+++ src/types/works.ts
@@ -5,21 +5,21 @@ export const works: Work[] = [
     title: `Curating memorable tech experiences with the Africa Startup Festival and the Africa Technology Expo. (Current)`,
     description:
       "I stay at the forefront of the brand communications for the Africa Startup Festival, and the Africa Technology Expo. Crafting design ideas, content and strategy framework, partnership, media, and influencer engagement. ",
-    image: "./../../public/images/startup.jpg",
+    image: "/images/startup.jpg",
     link: "https://www.africastartupfestival.com/en",
   },
   {
     title: "Hankara Bridges the Content Gap",
     description:
       "I collaborated with the founder of Hankara to enhance visibility, foster partnerships, and lead the team of three, while also managing two flagship initiatives: an accelerator program and a physical activation event.",
-    image: "./../../public/images/hanku.jpg",
+    image: "/images/hanku.jpg",
     link: "https://techcabal.com/2022/09/22/hankara-bridges-the-content-gap/",
   },
   {
     title: "Buffex: Money without Boundaries",
     description:
       "I collaborated with the founding team to develop the initial content strategy and product roadmap while contributing to early product concepts and features.",
-    image: "./../../public/images/buffex.jpg",
+    image: "/images/buffex.jpg",
     link: "https://vc4a.com/ventures/bufexx/",
   },
 ];
