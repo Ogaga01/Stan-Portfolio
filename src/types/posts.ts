4bd15754d947526a25021f9b911e221b423f3497
Ogaga <iyaraogaga69@gmail.com>
2025-02-25 08:45:39 +0100

    Latest update

--- src/types/posts.ts
+++ src/types/posts.ts
@@ -2,27 +2,27 @@ import { Post } from "./types";
 
 export const posts: Post[] = [
   {
-    title: "Embrace Wit, Skill, and Audacity This Year.",
-    image: "./../images/wit.jpg",
-    description: "do what thou must to be great.",
-    link: "https://theswiish.substack.com/p/embrace-wit-skill-and-audacity-this",
-  },
-  {
-    title: "Work Doctrine",
-    image: "./../images/work.jpg",
-    description: "some things that makes the work better.",
-    link: "https://theswiish.substack.com/p/work-doctrine",
-  },
-  {
     title: "Notes on Taste",
-    image: "./../images/note-on-taste.jpg",
+    image: "images/note-on-taste.jpg",
     description: "Why taste matters.",
     link: "https://theswiish.substack.com/p/notes-on-taste",
   },
   {
     title: '"Move Fast and Break Things" is Overrated',
-    image: "./../images/move-fast.jpg",
+    image: "images/move-fast.jpg",
     description: "On Building and Culture",
     link: "https://theswiish.substack.com/p/move-fast-and-break-things-is-overrated",
+  },
+  {
+    title: "Work Doctrine",
+    image: "/images/work-doctrine.jpg",
+    description: "some things that makes the work better.",
+    link: "https://theswiish.substack.com/p/work-doctrine",
+  },
+  {
+    title: "Embrace Wit, Skill, and Audacity This Year.",
+    image: "/images/wit.jpg",
+    description: "do what thou must to be great.",
+    link: "https://theswiish.substack.com/p/embrace-wit-skill-and-audacity-this",
   },
 ];
